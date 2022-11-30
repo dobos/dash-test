d82d47d9f88eefe6fa8984536fbfc768946a23c9
Laszlo Dobos <dobos@jhu.edu>
2022-11-30 11:09:10 -0500

    A few fixes.

--- python/dash_test/pages/tasks/index.py
+++ python/dash_test/pages/tasks/index.py
@@ -6,9 +6,12 @@ from dash.dash_table import DataTable
 dash.register_page(__name__, path="/tasks")
 
 def layout(**kwargs):
-    def get_task_link(r):
+    def get_task_link(task_id):
         href = dash.page_registry['pages.tasks.task']['relative_path']
-        return f'[Task {r}]({href}?task_id={r})'
+        # Generate markdown link in the form of
+        # [Task 12312434](task?task_id=12312434)
+        return f'[Task {task_id}]({href}?task_id={task_id})'
+        
 
     df = pd.DataFrame({
         'Date': ['2018-08-30 22:52:25', '2021-09-29 13:33:49'],
--- python/dash_test/pages/tasks/task.py
+++ python/dash_test/pages/tasks/task.py
@@ -3,7 +3,7 @@ from dash import html, dcc, callback, Input, Output
 
 dash.register_page(__name__)
 
-def layout(task_id, **kwargs):
+def layout(task_id=None, **kwargs):
     return html.Div([
         html.H1(f'This is our Task No. {task_id} page'),
         html.Div([
